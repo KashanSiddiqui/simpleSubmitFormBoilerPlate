77de27afe5a9d10c3aeee67d8a9e9be4bbca1cb8
salmank97 <salman5@live.com>
2019-02-23 18:45:36 +0500

    UI Fix

--- src/App.js
+++ src/App.js
@@ -1,153 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputBase from '@material-ui/core/InputBase';
-import InputLabel from '@material-ui/core/InputLabel';
-import TextField from '@material-ui/core/TextField';
-import FormControl from '@material-ui/core/FormControl';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
-import Button from '@material-ui/core/Button';
-import Icon from '@material-ui/core/Icon';
+import './App.css';
 
-const styles = theme => ({
-  button: {
-    margin: theme.spacing.unit,
-    margin:'0',
-    height:'53px'
-  },
-  rightIcon: {
-    marginLeft: theme.spacing.unit,
-  },
-  input: {
-    display: 'none',
-  },
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
 
-  margin: {
-    margin: theme.spacing.unit,
-  },
-  cssLabel: {
-    '&$cssFocused': {
-      color: purple[500],
-    },
-  },
-  cssFocused: {},
-  cssUnderline: {
-    '&:after': {
-      borderBottomColor: purple[500],
-    },
-  },
-  cssOutlinedInput: {
-    '&$cssFocused $notchedOutline': {
-      borderColor: purple[500],
-    },
-  },
-  notchedOutline: {},
-  bootstrapRoot: {
-    'label + &': {
-      marginTop: theme.spacing.unit * 3,
-    },
-  },
-  bootstrapInput: {
-    borderRadius: 4,
-    position: 'relative',
-    backgroundColor: theme.palette.common.white,
-    border: '1px solid #ced4da',
-    fontSize: 16,
-    width: 'auto',
-    padding: '10px 12px',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
-      borderRadius: 4,
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-    },
-  },
-  bootstrapFormLabel: {
-    fontSize: 18,
-  },
-});
-
-const theme = createMuiTheme({
-  palette: {
-    primary: green,
-  },
-  typography: { useNextVariants: true },
-});
-
-function CustomizedInputs(props) {
-  const { classes } = props;
-
+function Form(props) {
+  
   return (
-    <div className={classes.root} >
-      <div style={{margin:'0 auto'}}>
-      <TextField
-        className={classes.margin}
-        InputLabelProps={{
-          classes: {
-            root: classes.cssLabel,
-            focused: classes.cssFocused,
-          },
-        }}
-        InputProps={{
-          classes: {
-            root: classes.cssOutlinedInput,
-            focused: classes.cssFocused,
-            notchedOutline: classes.notchedOutline,
-          },
-        }}
-        label="INPUT ID"
-        variant="outlined"
-        id="custom-css-outlined-input"
-        style={{width:'25%',marginTop:'300px',marginLeft:'40px'}}
-      />
-      <div style={{marginTop:'25px'}}>
-      <input
-        accept="image/*"
-        className={classes.input}
-        id="contained-button-file"
-        multiple
-        type="file"
-        
-      />
-      <label htmlFor="contained-button-file" style={{margin:'0px'}}>
-        <Button variant="contained" component="span" className={classes.button}>
-          Upload
-        </Button>
-      </label>
-      </div>
-      <Button variant="contained" color="primary" className={classes.button} style={{marginTop:'25px',width:'105px'}}>
-        
-        SEND
-      </Button>
-      </div>
+
+    <div >
+      <form>
+        <div className="heading">
+          <p class="flow-text">Enroll Device</p>
+        </div>
+        <div className="form-style">
+          <div >
+            <input placeholder="Device Id" id="first_name" type="text" class="validate" />
+            <label for="first_name"></label>
+          </div>
+
+          <div class="file-field input-field">
+            <div class="btn">
+              <span>Public Key</span>
+              <input type="file" />
+            </div>
+            <div class="file-path-wrapper">
+              <input class="file-path validate" type="text" />
+            </div>
+          </div>
+
+          {/* <input type="file"></input> */}
+          <input type="submit" className=" btn waves-effect waves-light btn"></input>
+
+
+
+        </div>
+      </form>
     </div>
+
   );
 }
 
-CustomizedInputs.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-// ContainedButtons.propTypes = {
-// classes: PropTypes.object.isRequired,
-// };
-export default withStyles(styles)(CustomizedInputs);
+export default Form;
